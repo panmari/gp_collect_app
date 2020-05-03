6a312209679be16b88d6499b6e5d2e34d3b6fa59
panmari <smmuzi@gmail.com>
2020-05-03 16:42:01 +0200

    Fetching data on changing search term.

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json;
 
-void main() => runApp(CovidData());
+void main() => runApp(GPCollectData());
 
-class CovidDataState extends State<CovidData> {
-  Future<GPCollectData> futureData;
+class GPCollectDataState extends State<GPCollectData> {
+  Future<GPCollectSearchResult> _results;
   @override
   void initState() {
     super.initState();
-    futureData = fetchData();
+    _results = fetchData('');
   }
 
   // final _Font = const TextStyle(fontSize: 18.0);
@@ -17,24 +17,22 @@ class CovidDataState extends State<CovidData> {
     return ListTile(
       title: Text(
         runner.firstName + " " + runner.lastName,
-        // style: _Font,
       ),
       trailing: Text(
-        "more text",
-        // style: _biggerFont,
+        runner.fastestRunDuration, 
       ),
     );
   }
 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Data Example',
+      title: 'GP Bern runners',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Covid cases'),
+          title: Text('GP Bern runners'),
         ),
         body: Column(
           children: [
@@ -44,21 +42,23 @@ class CovidDataState extends State<CovidData> {
                 hintText: 'Enter a search term',
               ),
               onChanged: (text) {
-                print("First text field: ${text}");
+                setState(() {
+                  _results = fetchData(text);
+                });
               },
             ),
-            FutureBuilder<GPCollectData>(
-              future: futureData,
+            FutureBuilder<GPCollectSearchResult>(
+              future: _results,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return Expanded(
                       child: (ListView.builder(
-                        itemCount: snapshot.data.runners.length,
-                        itemBuilder: (context, index) {
-                          RunnerData runner = snapshot.data.runners[index];
-                          return _buildRow(runner);
-                        },
-                      )));
+                    itemCount: snapshot.data.runners.length,
+                    itemBuilder: (context, index) {
+                      RunnerData runner = snapshot.data.runners[index];
+                      return _buildRow(runner);
+                    },
+                  )));
                 } else if (snapshot.hasError) {
                   return Text("${snapshot.error}");
                 }
@@ -73,41 +73,42 @@ class CovidDataState extends State<CovidData> {
   }
 }
 
-class CovidData extends StatefulWidget {
+class GPCollectData extends StatefulWidget {
   @override
-  CovidDataState createState() => CovidDataState();
+  GPCollectDataState createState() => GPCollectDataState();
 }
 
 class RunnerData {
   final String firstName;
   final String lastName;
-  RunnerData({this.firstName, this.lastName});
+  final String fastestRunDuration;
+  RunnerData({this.firstName, this.lastName, this.fastestRunDuration});
 }
 
-class GPCollectData {
+class GPCollectSearchResult {
   final List<RunnerData> runners;
-  GPCollectData({this.runners});
+  GPCollectSearchResult({this.runners});
 
-  factory GPCollectData.fromJson(Map<String, dynamic> json) {
+  factory GPCollectSearchResult.fromJson(Map<String, dynamic> json) {
     List<RunnerData> runners = json['data'].map<RunnerData>((data) {
       return RunnerData(
         firstName: data["first_name"],
         lastName: data["last_name"],
+        fastestRunDuration: data["fastest_run_duration"],
       );
     }).toList();
-    return GPCollectData(
+    return GPCollectSearchResult(
       runners: runners,
     );
   }
 }
 
-Future<GPCollectData> fetchData() async {
-  final term = '';
+Future<GPCollectSearchResult> fetchData(searchTerm) async {
   final response = await http.get(
-      "http://gpcollect.duckdns.org/de/runners.json?search%5Bvalue%5D=${term}");
+    "http://gpcollect.duckdns.org/de/runners.json?draw=1&columns%5B0%5D%5Bdata%5D=first_name&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=last_name&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=club_or_hometown&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=sex&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=nationality&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=false&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=runs_count&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=fastest_run_duration&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=runner_id&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=10&search%5Bvalue%5D=$searchTerm");
   if (response.statusCode == 200) {
-    return GPCollectData.fromJson(json.decode(response.body));
+    return GPCollectSearchResult.fromJson(json.decode(response.body));
   } else {
-    throw Exception('Failed to load covid stats');
+    throw Exception('Failed to search runners, response: ${response.body}');
   }
 }
