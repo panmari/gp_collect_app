dbc3f92fe7158b073c7356e26956c24ba45b5f35
panmari <smmuzi@gmail.com>
2020-05-06 21:52:37 +0200

    Initial skeleton for adding interactivity.
    Needed some restructuring of code.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json;
 
-void main() => runApp(GPCollectData());
+void main() => runApp(GPCollectMain());
 
-class GPCollectDataState extends State<GPCollectData> {
+class GPCollectMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GP Bern runners',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: GPCollectDataWidget(),
+    );
+  }
+}
+
+class GPCollectDataWidget extends StatefulWidget {
+  @override
+  GPCollectDataState createState() => GPCollectDataState();
+}
+
+class GPCollectDataState extends State<GPCollectDataWidget> {
   Future<GPCollectSearchResult> _results;
   @override
-  void initState() {
+  void initState() { 
     super.initState();
-    _results = fetchData('');
+    _results = _fetchData('');
   }
 
-  // final _Font = const TextStyle(fontSize: 18.0);
+  Future<GPCollectSearchResult> _fetchData(searchTerm) async {
+    // TODO(panmari): Support pagination.
+    final response = await http
+        .get(
+            "http://gpcollect.duckdns.org/de/runners.json?draw=1&columns%5B0%5D%5Bdata%5D=first_name&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=last_name&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=club_or_hometown&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=sex&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=nationality&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=false&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=runs_count&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=fastest_run_duration&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=runner_id&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=50&search%5Bvalue%5D=$searchTerm")
+        .timeout(const Duration(seconds: 5));
+    if (response.statusCode == 200) {
+      return GPCollectSearchResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to search runners, response: ${response.body}');
+    }
+  }
+
   Widget _buildRow(RunnerData runner) {
     return ListTile(
       title: Text(
@@ -22,61 +52,54 @@ class GPCollectDataState extends State<GPCollectData> {
       trailing: Text(
         runner.fastestRunDuration,
       ),
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) {
+          return RunnerDetails();
+        }));
+      },
     );
   }
 
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GP Bern runners',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search for runners'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GP Bern runners'),
-        ),
-        body: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Enter a search term',
-              ),
-              onChanged: (text) {
-                setState(() {
-                  _results = fetchData(text);
-                });
-              },
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter a name or location',
             ),
-            FutureBuilder<GPCollectSearchResult>(
-              future: _results,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Expanded(
-                      child: (ListView.builder(
-                    itemCount: snapshot.data.runners.length,
-                    itemBuilder: (context, index) {
-                      RunnerData runner = snapshot.data.runners[index];
-                      return _buildRow(runner);
-                    },
-                  )));
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                // By default, show a loading spinner.
-                return CircularProgressIndicator();
-              },
-            ),
-          ],
-        ),
+            onChanged: (text) {
+              setState(() {
+                _results = _fetchData(text);
+              });
+            },
+          ),
+          FutureBuilder<GPCollectSearchResult>(
+            future: _results,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Expanded(
+                    child: (ListView.builder(
+                  itemCount: snapshot.data.runners.length,
+                  itemBuilder: (context, index) {
+                    RunnerData runner = snapshot.data.runners[index];
+                    return _buildRow(runner);
+                  },
+                )));
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ],
       ),
     );
   }
-}
-
-class GPCollectData extends StatefulWidget {
-  @override
-  GPCollectDataState createState() => GPCollectDataState();
 }
 
 class RunnerData {
@@ -115,13 +138,13 @@ class GPCollectSearchResult {
   }
 }
 
-Future<GPCollectSearchResult> fetchData(searchTerm) async {
-  // TODO(panmari): Support pagination.
-  final response = await http.get(
-      "http://gpcollect.duckdns.org/de/runners.json?draw=1&columns%5B0%5D%5Bdata%5D=first_name&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=last_name&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=club_or_hometown&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=sex&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=nationality&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=false&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=runs_count&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=fastest_run_duration&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=runner_id&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=50&search%5Bvalue%5D=$searchTerm");
-  if (response.statusCode == 200) {
-    return GPCollectSearchResult.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to search runners, response: ${response.body}');
+class RunnerDetails extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Page 2'),
+      ),
+    );
   }
 }
